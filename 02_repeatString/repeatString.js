a863cec246ccfddfc681756358709a9cd9234378
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-16 15:53:51 -0800

    Add code to repeat string a number of times (exercise 02)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, numRepeats) {
+    console.log(typeof numRepeats);
 
+    if (typeof numRepeats != 'number' || numRepeats < 0) return "ERROR";
+
+    let repeatString = "";
+    for (let i = 0; i < numRepeats; i++){
+        repeatString += string;
+    }
+    
+    return repeatString;
 };
 
 // Do not edit below this line
