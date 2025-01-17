1444aa3e742c8abc2a8ef1e848fc7ee570ddd14a
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-16 16:53:39 -0800

    Add function to convert Celsius to Fahrenheit (exercise 07)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,14 +1,19 @@
 const convertToCelsius = function(tempFahrenheit) {
-  // x Fahrenheit = (x - 32) * (5/9) Celcius
-  tempCelcius = (tempFahrenheit - 32) * (5/9);
-  if (!Number.isInteger(tempCelcius)) {
-    tempCelcius = Number(tempCelcius.toFixed(1));
+  // x Fahrenheit = (x - 32) * (5/9) Celsius
+  tempCelsius = (tempFahrenheit - 32) * (5/9);
+  if (!Number.isInteger(tempCelsius)) {
+    tempCelsius = Number(tempCelsius.toFixed(1));
   }
-  return tempCelcius;
+  return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
-
+const convertToFahrenheit = function(tempCelsius) {
+  // x Celsius = (9/5) * x + 32
+  tempFahrenheit = 9 / 5 * tempCelsius + 32;
+  if (!Number.isInteger(tempFahrenheit)) {
+    tempFahrenheit = Number(tempFahrenheit.toFixed(1));
+  }
+  return tempFahrenheit;
 };
 
 // Do not edit below this line
