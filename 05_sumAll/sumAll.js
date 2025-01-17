5a472107b5bdfc88fd87b4b7ca206563780f39dc
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-16 16:35:03 -0800

    Add function to sum integers between two given numbers (exercise 05)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+    // Use Gauss summation: sum of n consecutive numbers can be calculated by
+    // n/2 * (starting number + ending number)
 
+    if (typeof endInt != 'number' || typeof startInt != 'number'){
+        return 'ERROR';
+    }
+
+    if (startInt < 0 || endInt < 0){
+        return 'ERROR';
+    }
+
+    if (startInt != Math.floor(startInt) || endInt != Math.floor(endInt)){
+        return 'ERROR';
+    }
+
+    if (endInt < startInt){
+        let _ = endInt;
+        endInt = startInt;
+        startInt = _;
+    }
+
+    let numAddends = endInt - startInt + 1;
+    return numAddends/2 * (startInt + endInt);
 };
 
 // Do not edit below this line
