d1c1608d6915949c65be82d80b238df743bdfa5b
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-16 16:15:20 -0800

    Add function to remove specified elements from given array (exercise 04)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(origArray, ...toBeRemoved) {
+    revisedArray = [];
+    for (const element of origArray){
+        if (!toBeRemoved.includes(element)) {
+            revisedArray.push(element);
+        }
+    }
+    return revisedArray;
 };
 
 // Do not edit below this line
