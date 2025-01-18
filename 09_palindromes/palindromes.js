b7a1b8cf093b537701a600c6b56687477bd6b512
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-18 11:51:56 -0800

    Add palindrome checking function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
 
+    const letters = "1234567890abcdefghijklmnopqrstuvwxyz";
+    letterArr = string.split('').filter((char)=>letters.includes(char));
+
+    reversedLetterArr = letterArr.slice(0).reverse();
+
+    return letterArr.join('') === reversedLetterArr.join('');
 };
 
 // Do not edit below this line
