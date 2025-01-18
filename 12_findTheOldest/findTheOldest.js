07f313b6b273a5fe117de4fe615029174a6b59d8
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-18 12:26:46 -0800

    Add function to return older person object from an array of people

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let maxAge = -1;
+    let oldestPerson;
+    const currYear = new Date().getFullYear();
 
+    for (let person of people){
+
+        let age; 
+        if (person.yearOfDeath != undefined) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            age = currYear - person.yearOfBirth;
+        }
+
+        if (age > maxAge){
+            maxAge = age; 
+            oldestPerson = person;
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
