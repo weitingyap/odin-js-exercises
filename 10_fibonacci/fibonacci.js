028a8af982564700b3f6831cb2172b5c5bddc95b
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-18 12:08:07 -0800

    Add function to return array of book titles from array of objects

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
 
+    if ( n < 0 ) return 'OOPS';
+    if ( n === 0 ) return 0;
+    if ( n === 1 || n === 2 ) return 1;
+
+    let [secondPrevTerm, prevTerm] = [1, 1];
+
+    cnt = 3;
+    while ( cnt <= n ){
+        [secondPrevTerm, prevTerm] = [prevTerm, secondPrevTerm+prevTerm];
+        cnt++;
+    }
+    return prevTerm;
 };
 
 // Do not edit below this line
